498846a274df29d534aeb7aefe0d98a04a771bf5
Shreel Chhatbar <shreel.chhatbar@gmail.com>
2018-12-02 22:44:39 +0530

    Added gitconfig file | updated install script

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Installing all the dependencies "
 sudo apt install fonts-powerline exuberant-ctags cscope build-essential cmake3 cmake python3-dev ack-grep
 
-
 echo "Replacing the .vimrc files "
 rm ~/.vimrc
 ln ./rc_files/vimrc ~/.vimrc
@@ -27,6 +26,9 @@ ln ./syntax/c.vim ~/.vim/after/syntax/c.vim
 echo "Setting up the colorschemes "
 ln ./colors/heroku-terminal.vim ~/.vim/bundle/vim-colorschemes/colors/heroku-terminal.vim
 ln ./colors/badwolf.vim ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/badwolf.vim
+
+echo "Adding the git config"
+ln ./rc_files/gitconfig ~/.gitconfig
 
 echo "Installing YCM engine and updating it. "
 cd ~/.vim/bundle/YouCompleteMe
