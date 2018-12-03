3b86e27dc72e203ece247ac8f764cc65c2b19dda
Shreel Chhatbar <shreel.chhatbar@vvdntech.in>
2018-12-03 14:48:22 +0530

    More careful handling of the links, not gc

--- install.sh
+++ install.sh
@@ -11,27 +11,33 @@ sudo apt install fonts-powerline exuberant-ctags cscope build-essential cmake3 c
 echo "Replacing the .vimrc files "
 rm ~/.vimrc
 ln ./rc_files/vimrc ~/.vimrc
-chown nobody:nogroup ~/.viminfo
 
 echo "Installing all the plugins "
 vim +PluginInstall +qal
 
 echo "Putting in the .ackrc files "
+rm ~/.ackrc
 ln ./rc_files/ackrc ~/.ackrc
 
 echo "Installing the C syntax file "
+rm ~/.vim/after/syntax/c.vim
 mkdir -p ~/.vim/after/syntax/
 ln ./syntax/c.vim ~/.vim/after/syntax/c.vim
 
 echo "Setting up the colorschemes "
+rm ~/.vim/bundle/vim-colorschemes/colors/heroku-terminal.vim
 ln ./colors/heroku-terminal.vim ~/.vim/bundle/vim-colorschemes/colors/heroku-terminal.vim
+rm ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/badwolf.vim
 ln ./colors/badwolf.vim ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/badwolf.vim
+
+echo "Resetting .viminfo to remove ownership of root"
+rm ~/.viminfo
 
 #echo "Adding the git config"
 #ln ./rc_files/gitconfig ~/.gitconfig
 
 echo "Installing YCM engine and updating it. "
 cd ~/.vim/bundle/YouCompleteMe
-./install.py --all
+./install.py --clang-completer
 
 echo "*************Shreel's set up is complete ***************** "
