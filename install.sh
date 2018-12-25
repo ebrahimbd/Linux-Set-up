b0f015ecb7b614b088047ca587e6a586235a67c6
Shreel Chhatbar <shreel.chhatbar@gmail.com>
2018-12-25 13:54:38 +0530

    some changes for YT

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo $0: usage: sudo ./install.sh username
-    exit 1
-fi
+# if [ $# -ne 1 ]; then
+    # echo $0: usage: sudo ./install.sh username
+    # exit 1
+# fi
 
+uservar=$(whoami)
+echo "Installation being carried out for user $uservar"
 echo "*************Going to install the Shreel's set up***************** "
 echo "Installing Vundle "
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 #TODO : Need to seperate depencies according to the distro.
 echo "Installing all the dependencies "
-sudo apt install fonts-powerline exuberant-ctags cscope build-essential cmake3 cmake python3-dev ack-grep conky-all
+sudo apt install fonts-powerline exuberant-ctags cscope build-essential cmake python3-dev ack-grep
+# sudo apt install conky-all
 
 echo "Replacing the .vimrc files "
 rm ~/.vimrc
@@ -19,7 +22,7 @@ ln -s ~/Linux-Set-up/rc_files/vimrc ~/.vimrc
 
 # rm -rvf ~/.vim/undo
 mkdir ~/.vim/undo
-chown $1 ~/.vim/undo
+# chown uservar ~/.vim/undo
 
 echo "Installing all the plugins "
 vim +PluginInstall +qal
@@ -31,14 +34,14 @@ ln -s ~/Linux-Set-up/rc_files/ackrc ~/.ackrc
 echo "Copying Ultisnips"
 rm ~/.vim/my-snippets/c.snippets
 mkdir ~/.vim/my-snippets
-chown $1 ~/.vim/my-snippets
+# chown uservar ~/.vim/my-snippets
 ln -s ~/Linux-Set-up/ultisnips/c.snippets ~/.vim/my-snippets/c.snippets
 
 echo "Installing the C syntax file "
 rm ~/.vim/after/syntax/c.vim
 mkdir -p ~/.vim/after/syntax/
-chown $1 ~/.vim/after
-chown $1 ~/.vim/after/syntax
+# chown uservar ~/.vim/after
+# chown uservar ~/.vim/after/syntax
 ln -s ~/Linux-Set-up/syntax/c.vim ~/.vim/after/syntax/c.vim
 
 echo "Setting up the colorschemes "
@@ -48,24 +51,24 @@ rm ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/badwolf.vim
 ln -s ~/Linux-Set-up/colors/badwolf.vim ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/badwolf.vim
 
 echo "Resetting .viminfo to remove ownership of root"
-chown $1 ~/.viminfo
+# chown uservar ~/.viminfo
 
-echo "Putting in the .conkyrc files"
-rm ~/.conkyrc
-rm ~/.conkylogs
-ln -s ~/Linux-Set-up/rc_files/conkyrc ~/.conkyrc
-ln -s ~/Linux-Set-up/rc_files/conkylogs ~/.conkylogs
+# echo "Putting in the .conkyrc files"
+# rm ~/.conkyrc
+# rm ~/.conkylogs
+# ln -s ~/Linux-Set-up/rc_files/conkyrc ~/.conkyrc
+# ln -s ~/Linux-Set-up/rc_files/conkylogs ~/.conkylogs
 
-# newvar=/home/$1/Linux-Set-up/wallpaper/linux_sudo.png
+# newvar=/home/uservar/Linux-Set-up/wallpaper/linux_sudo.png
 # echo "Setting the wallpaper"
 # echo $newvar
-# sudo -u $1 gsettings set org.gnome.desktop.background picture-uri file://$newvar
+# sudo -u uservar gsettings set org.gnome.desktop.background picture-uri file://$newvar
 
 #echo "Adding the git config"
 #ln -s ~/Linux-Set-up/rc_files/gitconfig ~/.gitconfig
 
-# echo "Installing YCM engine and updating it. "
-# cd ~/.vim/bundle/YouCompleteMe
-# ./install.py --clang-completer
+echo "Installing YCM engine and updating it. "
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
 
 echo "*************Shreel's set up is complete ***************** "
