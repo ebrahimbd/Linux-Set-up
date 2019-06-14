131d3ddae1887ae1603b0befa5c2c77ae70a4148
Shreel <shreel.chhatbar@gmail.com>
2019-06-14 11:46:54 +0530

    Adding lock icon over the lock screen

--- scripts/customlock.sh
+++ scripts/customlock.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 shutter -f -e -o '/tmp/tmp.png'
-convert /tmp/tmp.png -blur 0x3 /tmp/lockpaper.png
+convert /tmp/tmp.png -blur 0x3 /tmp/lockpaper1.png
+composite -gravity center \( ~/Linux-Set-up/wallpaper/icon.png  -resize 90% \)  /tmp/lockpaper1.png  /tmp/lockpaper.png
 i3lock -i /tmp/lockpaper.png
 
