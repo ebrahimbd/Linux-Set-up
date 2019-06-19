f9f24e817b98581ef377929efd4d8d6b22dc4f59
Shreel <shreel.ac1991@gmail.com>
2019-06-19 10:00:25 +0530

    I can make errors too!

--- scripts/customlock.sh
+++ scripts/customlock.sh
@@ -2,6 +2,6 @@
 
 shutter -f -e -o '/tmp/tmp.png'
 convert /tmp/tmp.png -blur 0x3 /tmp/lockpaper1.png
-composite -gravity center \( ~/Linux-Set-up/icon/icon.png  -resize 90% \)  /tmp/lockpaper1.png  /tmp/lockpaper.png
+composite -gravity center \( ~/Linux-Set-up/.icons/icon.png  -resize 90% \)  /tmp/lockpaper1.png  /tmp/lockpaper.png
 i3lock -i /tmp/lockpaper.png
 
